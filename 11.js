3a8955e39e0cc9855ebf05a46c4dd14f99b6bdae
张藩 <zhangfan@moojnn.com>
2017-10-10 17:59:20 +0800

    2017/10/10 v1.0

---
+++ 11.js
@@ -0,0 +1,87 @@
+/**
+ * Created by fan on 2017/9/29.
+ */
+var fs = require('fs')
+	, file = "1.json"
+	, a = JSON.parse(fs.readFileSync( file))
+	, b = new Date()
+	, monthDay = []
+	, monthDays = 18
+	, monthTime = monthDays * 8 * 60
+;
+
+for(var Key in a[0]){
+	var day = []
+		,morning = a[0][Key].morning
+		, night = a[0][Key].night
+	;
+	day.push(morning.split(':'),night.split(':'));
+	monthDay.push(day);
+}
+var time = computTime(monthDay)
+	, elapsingTime = elapsedTime(time)
+	, remainingTime = timeRemain(time, monthTime)
+	, leftDay = monthDays - monthDay.length - 1
+	, leftTime = monthTime - time
+	, extraTime = extraDayTime(leftDay, leftTime)
+;
+console.log(time, leftTime, monthTime);
+console.log('已经上班：',elapsingTime,'还剩时间：', remainingTime, '剩下每天需要多上多长时间：',extraTime);
+
+
+
+/**
+ * 计算时间
+ * @param arr
+ */
+function computTime(arr) {
+	var timeAmout = 0;
+	for(var i = 0; i < arr.length; i++){
+		var day = arr[i]
+			, morningTime = parseInt(day[0][0]) * 60 + parseInt(day[0][1])
+			, nightTime = parseInt(day[1][0]) * 60 + parseInt(day[1][1])
+			, time = nightTime - morningTime
+		;
+		if(time > 3 * 60){
+			time = time - 60;
+		}
+		else if(nightTime - morningTime > 11 * 60){
+			time -= 90;
+		}
+		timeAmout += time;
+	}
+	return timeAmout;
+}
+
+/**
+ * 格式化成小时模式
+ * @param time
+ */
+function elapsedTime(time) {
+	var eTime
+		, mTime = time % 60 < 10 ? '0' + time % 60 : time % 60
+	;
+	eTime = Math.floor(time / 60) + ':' + mTime;
+	return eTime ;
+}
+
+/**
+ * 剩余时间
+ * @param time
+ * @param allTime
+ * @returns {string|*}
+ */
+function timeRemain(time, allTime) {
+	var leftTime = allTime - time
+		, mTime = leftTime % 60 < 10 ? '0' + leftTime % 60 : leftTime % 60
+		, rTime
+	;
+	rTime = Math.floor(leftTime / 60) + ':' + mTime;
+	return rTime;
+}
+
+function extraDayTime(leftDay, leftTime) {
+	console.log(leftDay, leftTime);
+	var dayTime = leftTime / leftDay / 60 - 8;
+	return dayTime * 60;
+}
